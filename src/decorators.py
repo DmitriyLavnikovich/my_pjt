f0949414035184c8d3ca32a4ba7b981aec019f02
Дмитрий Лавникович <Lavnikovich.dm@yandex.ru>
2025-01-27 21:02:50 +0700

    correction_1

--- src/decorators.py
+++ src/decorators.py
@@ -1,5 +1,36 @@
 from functools import wraps
 
+from pyexpat.errors import messages
+
+
+# def log(filename = None):
+#     """ "декоратор, логирующий начало и конец выполнения функции, а также ее результаты или возникшие ошибки"""
+#
+#     def my_decorator(func):
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             try:
+#                 if filename:
+#                     with open(filename, "a") as file:
+#                         file.write(f"{func.__name__} ok\n")
+#                 else:
+#                     print(f"{func.__name__} ok")
+#
+#                 result = func(*args, **kwargs)
+#
+#             except Exception as error:
+#                 error_massage = f"{func.__name__} error TypeError"
+#                 if filename:
+#                     with open(filename, "a") as file:
+#                         file.write(error_massage + "\n")
+#                 else:
+#                     print(error_massage)
+#                 raise
+#             return result
+#
+#         return wrapper
+#
+#     return my_decorator
 
 def log(filename = None):
     """ "декоратор, логирующий начало и конец выполнения функции, а также ее результаты или возникшие ошибки"""
@@ -8,24 +39,21 @@ def log(filename = None):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                if filename:
-                    with open(filename, "a") as file:
-                        file.write(f"{func.__name__} ok\n")
-                else:
-                    print(f"{func.__name__} ok")
-
                 result = func(*args, **kwargs)
-
-            except Exception as error:
+                print(f"{func.__name__} ok")
+            except Exception as e:
                 error_massage = f"{func.__name__} error TypeError"
-                if filename:
-                    with open(filename, "a") as file:
-                        file.write(error_massage + "\n")
-                else:
-                    print(error_massage)
+                print(error_massage)
                 raise
             return result
-
+        if filename:
+            try:
+                with open(filename, "a") as file:
+                    file.write(f"{func.__name__} ok\n")
+            except:
+                error_massage = f"{func.__name__} error TypeError"
+                with open(filename, "a") as file:
+                    file.write(error_massage + "\n")
         return wrapper
 
     return my_decorator
